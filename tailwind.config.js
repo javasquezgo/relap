d27669384d1ca6087ef7d0e149d8cf762a4dd6ec
Alejandro Vasquez <josejaguer12@gmail.com>
2021-04-15 18:37:37 -0600

    Primera seccion hecha

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        'poppins':['Poppins']
+      },
+      colors:{
+        'black-grey':'#333',
+        'veish':'#fdf2ff'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
